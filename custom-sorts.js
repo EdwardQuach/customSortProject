9af03b4633c6d4e52257455e913d658e298efc7e
ben waldee <b1.waldee@gmail.com>
2022-06-14 15:03:18 -0500

    02 done

--- custom-sorts.js
+++ custom-sorts.js
@@ -3,7 +3,27 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  let oddArr = []
+  let evenArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    let ele = arr[i]
+    if (ele % 2 === 0) { evenArr.push(ele) }
+    else { oddArr.push(ele) }
+  }
+
+  let sortedOdd = oddArr.sort((a, b) => a - b)
+  let sortedEven = evenArr.sort((a, b) => a - b)
+
+  return [...sortedOdd, ...sortedEven]
+
+  // return arr.sort((a, b) => {
+  //   if (a % 2 === 0 && b % 2 === 0) { return a - b }
+  //   else if (a % 2 !== 0 && b % 2 !== 0) { return a - b }
+  //   else if (a % 2 !== 0 && b % 2 === 0) { return -1 }
+  //   else if (a % 2 === 0 && b % 2 !== 0) { return 1 }
+  // })
+
 }
 
 function validAnagrams(s, t) {
